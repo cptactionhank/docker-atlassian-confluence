c84f8456e46cf1e19e17ea3e8d4b7d606b682c51
Martin Jensen <maage@dotmaage.dk>
2014-10-26 00:23:56 +0200

    Extended docker integration tests to require minimum version and api version

--- spec/docker_spec.rb
+++ spec/docker_spec.rb
@@ -17,19 +17,27 @@ describe "Docker image building" do
 
   context "docker is working" do
 
-    it "should have an url" do
+    it "should have an URL" do
       expect(Docker.url).to_not be_nil
     end
 
-    it "should have version informtaion" do
+    it "should have version information" do
       expect(Docker.version).to_not be_nil
+    end
+
+    it "version should be newer or equal to 1.3.0" do
+      Gem::Version.new(Docker::version["Version"]) >= Gem::Version.new('1.3.0')
+    end
+
+    it "API version should be newer or equal to 15" do
+      Gem::Version.new(Docker::version["ApiVersion"]) >= Gem::Version.new('15')
     end
 
   end
 
   context "building test image" do
 
-      it "should succesfully build app image" do
+      it "should successfully build application image" do
         $image = Docker::Image.build_from_dir "."
         expect($image).to_not be_nil
       end
@@ -40,7 +48,7 @@ describe "Docker image building" do
 
   end
 
-  context "creating and starting app container" do
+  context "creating and starting application container" do
 
       it "should succeed creating container" do
         $container = Docker::Container.create Image: $image.id
