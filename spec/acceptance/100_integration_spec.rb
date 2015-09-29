ee04945c6bde877838698aca45edcf18f0c48317
Martin Jensen <maage@dotmaage.dk>
2015-09-29 07:58:30 +0200

    Enables embedded, postgres, and mysql acceptance tests

--- spec/acceptance/100_integration_spec.rb
+++ spec/acceptance/100_integration_spec.rb
@@ -1,32 +1,32 @@
 require 'timeout'
 require 'spec_helper'
 
-# describe 'Atlassian Confluence with Embedded Database', order: :defined do
-# 	include_examples 'an acceptable confluence instance', 'using an embedded database'
-# end
+describe 'Atlassian Confluence with Embedded Database', order: :defined do
+	include_examples 'an acceptable confluence instance', 'using an embedded database'
+end
 
-# describe 'Atlassian Confluence with PostgreSQL 9.3 Database', order: :defined do
-# 	include_examples 'an acceptable confluence instance', 'using a postgresql database' do
-# 		unless ENV["CI"] == "true"
-# 			before :all do
-# 				# Create and run a PostgreSQL 9.3 container instance
-# 				@container_db = Docker::Image.create('fromImage' => 'postgres', 'tag' => '9.3').run
-# 				# Wait for the PostgreSQL instance to start
-# 				@container_db.wait_for_output %r{PostgreSQL\ init\ process\ complete;\ ready\ for\ start\ up\.}
-# 				# Create Confluence database
-# 				@container_db.exec ["psql", "--username", "postgres", "--command", "create database confluence owner postgres encoding 'utf8';"]
-# 			end
-# 		else
-# 			before :all do
-# 				@container_db = Docker::Container.get 'postgres'
-# 			end
-# 		end
+describe 'Atlassian Confluence with PostgreSQL 9.3 Database', order: :defined do
+	include_examples 'an acceptable confluence instance', 'using a postgresql database' do
+		unless ENV["CI"] == "true"
+			before :all do
+				# Create and run a PostgreSQL 9.3 container instance
+				@container_db = Docker::Image.create('fromImage' => 'postgres', 'tag' => '9.3').run
+				# Wait for the PostgreSQL instance to start
+				@container_db.wait_for_output %r{PostgreSQL\ init\ process\ complete;\ ready\ for\ start\ up\.}
+				# Create Confluence database
+				@container_db.exec ["psql", "--username", "postgres", "--command", "create database confluence owner postgres encoding 'utf8';"]
+			end
+		else
+			before :all do
+				@container_db = Docker::Container.get 'postgres'
+			end
+		end
 
-# 		after :all do
-# 			@container_db.remove force: true, v: true unless @container_db.nil?
-# 		end
-# 	end
-# end
+		after :all do
+			@container_db.remove force: true, v: true unless @container_db.nil?
+		end
+	end
+end
 
 describe 'Atlassian Confluence with MySQL 5.6 Database', order: :defined do
 	include_examples 'an acceptable confluence instance', 'using a mysql database' do
