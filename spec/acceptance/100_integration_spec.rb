88c1e8ad584ae4ee725855fc512224cb73171544
Martin Jensen <maage@dotmaage.dk>
2015-09-29 23:36:34 +0200

    moved to use global instead of instance variables for database container access

--- spec/acceptance/100_integration_spec.rb
+++ spec/acceptance/100_integration_spec.rb
@@ -10,20 +10,20 @@ describe 'Atlassian Confluence with PostgreSQL 9.3 Database', order: :defined do
 		unless ENV["CI"] == "true"
 			before :all do
 				# Create and run a PostgreSQL 9.3 container instance
-				@container_db = Docker::Image.create('fromImage' => 'postgres', 'tag' => '9.3').run
+				$container_db = Docker::Image.create('fromImage' => 'postgres', 'tag' => '9.3').run
 				# Wait for the PostgreSQL instance to start
-				@container_db.wait_for_output %r{PostgreSQL\ init\ process\ complete;\ ready\ for\ start\ up\.}
+				$container_db.wait_for_output %r{PostgreSQL\ init\ process\ complete;\ ready\ for\ start\ up\.}
 				# Create Confluence database
-				@container_db.exec ["psql", "--username", "postgres", "--command", "create database confluence owner postgres encoding 'utf8';"]
+				$container_db.exec ["psql", "--username", "postgres", "--command", "create database confluence owner postgres encoding 'utf8';"]
 			end
 		else
 			before :all do
-				@container_db = Docker::Container.get 'postgres'
+				$container_db = Docker::Container.get 'postgres'
 			end
 		end
 
 		after :all do
-			@container_db.remove force: true, v: true unless @container_db.nil?
+			$container_db.remove force: true, v: true unless $container_db.nil?
 		end
 	end
 end
@@ -34,19 +34,19 @@ describe 'Atlassian Confluence with MySQL 5.6 Database', order: :defined do
 			before :all do
 				# Create and run a MySQL 5.6 container instance
 				image = Docker::Image.create('fromImage' => 'mysql', 'tag' => '5.6')
-				@container_db = Docker::Container.create 'Image' => image.id, 'Env' => ["MYSQL_ROOT_PASSWORD=mysecretpassword"]
-				@container_db.start!
+				$container_db = Docker::Container.create 'Image' => image.id, 'Env' => ["MYSQL_ROOT_PASSWORD=mysecretpassword"]
+				$container_db.start!
 				# Wait for the MySQL instance to start
-				@container_db.wait_for_output %r{socket:\ '/var/run/mysqld/mysqld\.sock'\ \ port:\ 3306\ \ MySQL\ Community\ Server\ \(GPL\)}
+				$container_db.wait_for_output %r{socket:\ '/var/run/mysqld/mysqld\.sock'\ \ port:\ 3306\ \ MySQL\ Community\ Server\ \(GPL\)}
 				# Create Confluence database
-				@container_db.exec ['mysql', '--user=root', '--password=mysecretpassword', '--execute', 'CREATE DATABASE confluence CHARACTER SET utf8 COLLATE utf8_bin;']
+				$container_db.exec ['mysql', '--user=root', '--password=mysecretpassword', '--execute', 'CREATE DATABASE confluence CHARACTER SET utf8 COLLATE utf8_bin;']
 			end
 		else
-			@container_db = Docker::Container.get 'mysql'
+			$container_db = Docker::Container.get 'mysql'
 		end
 
 		after :all do
-			@container_db.remove force: true, v: true unless @container_db.nil?
+			$container_db.remove force: true, v: true unless $container_db.nil?
 		end
 	end
 end
--- spec/support/shared_examples/using_a_postgresql_database_shared_example.rb
+++ spec/support/shared_examples/using_a_postgresql_database_shared_example.rb
@@ -28,7 +28,7 @@ shared_examples 'using a postgresql database' do
 	describe 'setting up JDBC Configuration' do
 		before :all do
 			within 'form[name=dbform]' do
-				fill_in 'dbConfigInfo.databaseUrl', with: 'jdbc:postgresql://localhost:5432/confluence'.gsub('localhost', @container_db.host)
+				fill_in 'dbConfigInfo.databaseUrl', with: 'jdbc:postgresql://localhost:5432/confluence'.gsub('localhost', $container_db.host)
 				fill_in 'dbConfigInfo.userName', with: 'postgres'
 				fill_in 'dbConfigInfo.password', with: 'mysecretpassword'
 				click_button 'Next'
