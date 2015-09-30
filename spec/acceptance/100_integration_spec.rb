17ec080ff2c7a25ebff2d62b73b184bcc255dcde
Martin Jensen <maage@dotmaage.dk>
2015-09-30 23:19:52 +0200

    Yet another whack at getting multiple database acceptance tests

--- spec/acceptance/100_integration_spec.rb
+++ spec/acceptance/100_integration_spec.rb
@@ -10,20 +10,20 @@ describe 'Atlassian Confluence with PostgreSQL 9.3 Database', order: :defined do
 		unless ENV["CI"] == "true"
 			before :all do
 				# Create and run a PostgreSQL 9.3 container instance
-				@container_postgres = Docker::Image.create('fromImage' => 'postgres', 'tag' => '9.3').run
+				$container_postgres = Docker::Image.create('fromImage' => 'postgres', 'tag' => '9.3').run
 				# Wait for the PostgreSQL instance to start
-				@container_postgres.wait_for_output %r{PostgreSQL\ init\ process\ complete;\ ready\ for\ start\ up\.}
+				$container_postgres.wait_for_output %r{PostgreSQL\ init\ process\ complete;\ ready\ for\ start\ up\.}
 				# Create Confluence database
-				@container_postgres.exec ["psql", "--username", "postgres", "--command", "create database confluence owner postgres encoding 'utf8';"]
+				$container_postgres.exec ["psql", "--username", "postgres", "--command", "create database confluence owner postgres encoding 'utf8';"]
 			end
 		else
 			before :all do
-				@container_postgres = Docker::Container.get 'postgres'
+				$container_postgres = Docker::Container.get 'postgres'
 			end
 		end
 
 		after :all do
-			@container_postgres.remove force: true, v: true unless @container_postgres.nil?
+			$container_postgres.remove force: true, v: true unless $container_postgres.nil?
 		end
 	end
 end
@@ -34,19 +34,19 @@ describe 'Atlassian Confluence with MySQL 5.6 Database', order: :defined do
 			before :all do
 				# Create and run a MySQL 5.6 container instance
 				image = Docker::Image.create('fromImage' => 'mysql', 'tag' => '5.6')
-				@container_mysql = Docker::Container.create 'Image' => image.id, 'Env' => ["MYSQL_ROOT_PASSWORD=mysecretpassword"]
-				@container_mysql.start!
+				$container_mysql = Docker::Container.create 'Image' => image.id, 'Env' => ["MYSQL_ROOT_PASSWORD=mysecretpassword"]
+				$container_mysql.start!
 				# Wait for the MySQL instance to start
-				@container_mysql.wait_for_output %r{socket:\ '/var/run/mysqld/mysqld\.sock'\ \ port:\ 3306\ \ MySQL\ Community\ Server\ \(GPL\)}
+				$container_mysql.wait_for_output %r{socket:\ '/var/run/mysqld/mysqld\.sock'\ \ port:\ 3306\ \ MySQL\ Community\ Server\ \(GPL\)}
 				# Create Confluence database
-				@container_mysql.exec ['mysql', '--user=root', '--password=mysecretpassword', '--execute', 'CREATE DATABASE confluence CHARACTER SET utf8 COLLATE utf8_bin;']
+				$container_mysql.exec ['mysql', '--user=root', '--password=mysecretpassword', '--execute', 'CREATE DATABASE confluence CHARACTER SET utf8 COLLATE utf8_bin;']
 			end
 		else
-			@container_mysql = Docker::Container.get 'mysql'
+			$container_mysql = Docker::Container.get 'mysql'
 		end
 
 		after :all do
-			@container_mysql.remove force: true, v: true unless @container_mysql.nil?
+			$container_mysql.remove force: true, v: true unless $container_mysql.nil?
 		end
 	end
 end
