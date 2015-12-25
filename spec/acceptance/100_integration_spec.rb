18d4226dacb96307b97981acbb8af1a054ae34a6
Circle CI Automated Builder <circleci@circleci>
2015-12-25 23:24:43 +0100

    Updated acceptance tests

--- spec/acceptance/100_integration_spec.rb
+++ spec/acceptance/100_integration_spec.rb
@@ -1,15 +1,14 @@
 require 'timeout'
 require 'spec_helper'
 
-describe 'Atlassian Confluence with Embedded Database' do
+describe 'Atlassian Confluence with Embedded Database', order: :defined do
   include_examples 'an acceptable Confluence instance', 'using an embedded database'
 end
 
-describe 'Atlassian Confluence with PostgreSQL 9.3 Database' do
+describe 'Atlassian Confluence with PostgreSQL 9.3 Database', order: :defined do
   include_examples 'an acceptable Confluence instance', 'using a postgresql database' do
     unless ENV["CI"] == "true"
       before :all do
-        Docker::Image.create fromImage: 'postgres:9.3'
         # Create and run a PostgreSQL 9.3 container instance
         $container_postgres = Docker::Container.create image: 'postgres:9.3'
         $container_postgres.start!
@@ -18,23 +17,21 @@ describe 'Atlassian Confluence with PostgreSQL 9.3 Database' do
         # Create Confluence database
         $container_postgres.exec ["psql", "--username", "postgres", "--command", "create database confluencedb owner postgres encoding 'utf8';"]
       end
+      after :all do
+        $container_postgres.remove force: true, v: true unless $container_postgres.nil?
+      end
     else
       before :all do
         $container_postgres = Docker::Container.get 'postgres'
       end
     end
-
-    after :all do
-      $container_postgres.remove force: true, v: true unless $container_postgres.nil? || ENV["CI"] == "true"
-    end
   end
 end
 
-describe 'Atlassian Confluence with MySQL 5.6 Database' do
+describe 'Atlassian Confluence with MySQL 5.6 Database', order: :defined do
   include_examples 'an acceptable Confluence instance', 'using a mysql database' do
     unless ENV["CI"] == "true"
       before :all do
-        Docker::Image.create fromImage: 'mysql:5.6'
         # Create and run a MySQL 5.6 container instance
         $container_mysql = Docker::Container.create image: 'mysql:5.6', env: ['MYSQL_ROOT_PASSWORD=mysecretpassword']
         $container_mysql.start!
@@ -44,7 +41,7 @@ describe 'Atlassian Confluence with MySQL 5.6 Database' do
         $container_mysql.exec ['mysql', '--user=root', '--password=mysecretpassword', '--execute', 'CREATE DATABASE confluencedb CHARACTER SET utf8 COLLATE utf8_bin;']
       end
       after :all do
-        $container_mysql.remove force: true, v: true unless $container_mysql.nil? || ENV["CI"] == "true"
+        $container_mysql.remove force: true, v: true unless $container_mysql.nil?
       end
     else
       $container_mysql = Docker::Container.get 'mysql'
