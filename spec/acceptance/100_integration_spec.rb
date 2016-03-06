bde00d965f59fd89ef2e31ad40b67d30bbc8252e
Circle CI Automated Builder <circleci@circleci>
2016-03-06 08:50:46 +0100

    extended expected Travis CI build time

--- spec/acceptance/100_integration_spec.rb
+++ spec/acceptance/100_integration_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe 'Atlassian Confluence with Embedded Database', order: :defined do
 
-	include_examples 'a buildable docker image', '.', { Env: ["CATALINA_OPTS=-Xms1024m -Xmx2048m -Datlassian.plugins.enable.wait=#{Docker::DSL.timeout}"] }
+	include_examples 'a buildable docker image', '.', { env: ["CATALINA_OPTS=-Xms2048m -Xmx3072m -Datlassian.plugins.enable.wait=#{Docker::DSL.timeout}"] }
 
 	include_examples 'an acceptable confluence instance', 'using an embedded database'
 
@@ -11,7 +11,7 @@ end
 
 describe 'Atlassian Confluence with PostgreSQL 9.3 Database', order: :defined do
 
-	include_examples 'a buildable docker image', '.', { Env: ["CATALINA_OPTS=-Xms1024m -Xmx2048m -Datlassian.plugins.enable.wait=#{Docker::DSL.timeout}"] }
+	include_examples 'a buildable docker image', '.', { env: ["CATALINA_OPTS=-Xms2048m -Xmx3072m -Datlassian.plugins.enable.wait=#{Docker::DSL.timeout}"] }
 
 	include_examples 'an acceptable confluence instance', 'using a postgresql database' do
 		before :all do
@@ -36,7 +36,7 @@ end
 
 describe 'Atlassian Confluence with MySQL 5.6 Database', order: :defined do
 
-	include_examples 'a buildable docker image', '.', { Env: ["CATALINA_OPTS=-Xms1024m -Xmx2048m -Datlassian.plugins.enable.wait=#{Docker::DSL.timeout}"] }
+	include_examples 'a buildable docker image', '.', { env: ["CATALINA_OPTS=-Xms2048m -Xmx3072m -Datlassian.plugins.enable.wait=#{Docker::DSL.timeout}"] }
 
 	include_examples 'an acceptable confluence instance', 'using a mysql database' do
 		before :all do
@@ -62,8 +62,8 @@ end
 describe 'Atlassian Confluence behind reverse proxy', order: :defined do
 
 	include_examples 'a buildable docker image', '.', {
-		Env: [
-			"CATALINA_OPTS=-Xms1024m -Xmx2048m -Datlassian.plugins.enable.wait=#{Docker::DSL.timeout}",
+		env: [
+			"CATALINA_OPTS=-Xms2048m -Xmx3072m -Datlassian.plugins.enable.wait=#{Docker::DSL.timeout}",
 			"X_PROXY_NAME=#{Docker.info['Name']}",
 			'X_PROXY_PORT=80',
 			'X_PROXY_SCHEME=http',
@@ -76,7 +76,7 @@ describe 'Atlassian Confluence behind reverse proxy', order: :defined do
 			@container_proxy = Docker::Container.create image: 'blacklabelops/nginx:latest',
 				PortBindings: { '8080/tcp': [{ 'HostPort': '80' }] },
 				Links: ["#{@container.id}:container"],
-				Env: ['SERVER1REVERSE_PROXY_LOCATION1=/confluence/', 'SERVER1REVERSE_PROXY_PASS1=http://container:8090/confluence/']
+				env: ['SERVER1REVERSE_PROXY_LOCATION1=/confluence/', 'SERVER1REVERSE_PROXY_PASS1=http://container:8090/confluence/']
 			@container_proxy.start!
 			@container_proxy.setup_capybara_url({ tcp: 8080 }, '/confluence/')
 		end
