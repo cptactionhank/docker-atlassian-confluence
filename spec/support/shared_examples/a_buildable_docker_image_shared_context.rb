0dc66d5311022d5ac0805e87ceacea3d0858b753
Circle CI Automated Builder <circleci@circleci>
2016-03-17 23:20:11 +0000

    Created new Atlassian Confluence branch for version 5.9.7

---
+++ spec/support/shared_examples/a_buildable_docker_image_shared_context.rb
@@ -0,0 +1,19 @@
+shared_examples 'a buildable docker image' do |path, options = {}|
+  subject { @container }
+
+  before :all do
+    image = Docker::Image.build_from_dir(path)
+    container_options = { Image: image.id }.merge options
+    @container = Docker::Container.create container_options
+    @container.start! PublishAllPorts: true
+    @container.setup_capybara_url tcp: 8090
+  end
+
+  after :all do
+    if ENV['CIRCLECI']
+      @container.kill signal: 'SIGKILL'
+    else
+      @container.remove force: true, v: true
+    end
+  end
+end
