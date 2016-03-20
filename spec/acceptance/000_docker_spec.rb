d410dca570ae84eddac45a1c7c07e450290331eb
Circle CI Automated Builder <circleci@circleci>
2016-03-20 17:16:32 +0100

    Stylistic refactoring of Ruby code and added proxy support

--- spec/acceptance/000_docker_spec.rb
+++ spec/acceptance/000_docker_spec.rb
@@ -1,6 +1,3 @@
-require 'docker'
-require 'spec_helper'
-
 describe 'Docker image building' do
   context 'when validating host software' do
     it 'should supported version' do
@@ -15,7 +12,7 @@ describe 'Docker image building' do
     it { is_expected.to have_exposed_port tcp: 8090 }
     it { is_expected.to_not have_exposed_port udp: 8090 }
     it { is_expected.to have_volume '/var/atlassian/confluence' }
-    it { is_expected.not_to have_volume '/opt/atlassian/confluence' }
+    it { is_expected.to have_volume '/opt/atlassian/confluence/logs' }
     it { is_expected.to have_working_directory '/var/atlassian/confluence' }
   end
 end
